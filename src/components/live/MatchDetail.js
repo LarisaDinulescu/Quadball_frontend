1171d37b92c88d90a4d3a3f579e66c448cc5821f
elenapozzebon <896456@stud.unive.it>
2026-02-02 17:01:37 +0100

    Live Matches: update the logic with websocket

--- src/components/live/MatchDetail.js
+++ src/components/live/MatchDetail.js
@@ -174,7 +174,7 @@ const MatchDetail = () => {
                             ) : (
                                 <>
                                     <span className="relative flex h-3 w-3"><span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span><span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span></span>
-                                    <span className="text-xs font-black text-red-600 tracking-widest uppercase">LIVE SIMULATION</span>
+                                    <span className="text-xs font-black text-red-600 tracking-widest uppercase">LIVE</span>
                                 </>
                             )}
                         </div>
